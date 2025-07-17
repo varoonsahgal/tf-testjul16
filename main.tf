acaa6f986042a032a5f478c3e919bd9194770bc7
varoonsahgal <varoon.sahgal@gmail.com>
2025-07-17 15:43:05 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,61 +15,8 @@ terraform {
 
 #kick off pipeline
 resource "azurerm_resource_group" "main" {
-  name     = "myTFResourceGroupVSjul16"
+  name     = "myTFResourceGroupVSjul17"
   location = "westus2"
-}
-
-resource "azurerm_virtual_network" "main" {
-  name                = "myTFVNet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-}
-
-resource "azurerm_subnet" "main" {
-  name                 = "myTFSubnet"
-  resource_group_name  = azurerm_resource_group.main.name
-  virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_network_interface" "main" {
-  name                = "myTFNIC"
-  location            = azurerm_resource_group.main.location
-  resource_group_name = azurerm_resource_group.main.name
-
-  ip_configuration {
-    name                          = "myIPConfig"
-    subnet_id                     = azurerm_subnet.main.id
-    private_ip_address_allocation = "Dynamic"
-  }
-}
-
-resource "azurerm_linux_virtual_machine" "main" {
-  name                = "myTFVM"
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-  size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.main.id,
-  ]
-  admin_password = "P@ssword1234!"
-  disable_password_authentication = false
-
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    name                 = "myosdisk"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
 }
 
 

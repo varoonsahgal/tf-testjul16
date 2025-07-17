e1fc5bf809e98c8b16456a68d23f5e3258d38182
varoonsahgal <varoon.sahgal@gmail.com>
2025-07-17 12:48:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,16 @@ provider "azurerm" {
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc"
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "my-rg-vsahgal"
+    storage_account_name = "mytfstateacctvsahgal"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
 #kick off pipeline
 resource "azurerm_resource_group" "main" {
   name     = "myTFResourceGroupVSjul16"
